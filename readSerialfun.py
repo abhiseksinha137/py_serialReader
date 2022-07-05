31e53ce7cd8d3afa09216d2b966a3784312d513f
Abhisek Sinha <sinha.abhisek92@gmail.com>
2022-07-04 21:02:07 -0700

    added

--- readSerialfun.py
+++ readSerialfun.py
@@ -1,5 +1,13 @@
 import serial
-ser = serial.Serial("COM11", 9600)
+import time
+ser = serial.Serial("COM3", 115200)
 while True:
      cc=str(ser.readline())
-     print(cc[2:][:-5])
+     val=cc[2:][:-5]
+     parts=val.replace('\\t', ',')
+     part1=parts[0]
+     part2=parts[1]
+
+     # print(part1, part2)
+     print(parts)
+     time.sleep(1)
